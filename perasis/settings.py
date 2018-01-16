ac562c5d4e2bd0aa25502445319d2af85616e6d7
Goran <goranaviani@gmail.com>
2018-01-16 18:47:07 +0100

    setting.py

--- perasis/settings.py
+++ perasis/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 STATIC_URL = '/static_files/'
 
 #for deployment uncomment or for local comment:
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static_files/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files/')
 ###PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 ####STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, "static_files"), )
 
@@ -151,10 +151,12 @@ STATIC_URL = '/static_files/'
 
 #added for static files ->
 #for deployment comment for local uncommenr:
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static_files'),
-)
+
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, 'static_files'),
+#)
+
 #added for static files
 
 
