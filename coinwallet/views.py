eaa2ffc0893c96247e6065db4f4173b9700b1e3d
Goran <goranaviani@gmail.com>
2018-01-29 21:59:33 +0100

    crypto coin wallet first version

--- coinwallet/views.py
+++ coinwallet/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from coinwallet.models import CoinWallet
 
+import os
+import json
+import time
+
+
 from coinwallet.forms import (
 CoinWalletCreateForm,
 )
@@ -9,16 +14,55 @@ CoinWalletCreateForm,
 # Create your views here.
 def CoinWalletDashboardView(request):
     if request.user.is_authenticated():
+            os.system("curl -X GET https://api.coinmarketcap.com/v1/ticker/?limit=0 > prices.txt")
+            wallet_sum=0
+            wallet_sum_per_coin={}
+            with open('prices.txt', 'r') as content_file:
+                content = content_file.read()
+
+                sve = json.loads(content)
+                queryCoinWallet_model = CoinWallet.objects.all()
+                for x in queryCoinWallet_model:
+                    print(x)
+                    for valuta in sve:
+                        #print(valuta['symbol'])
+                        if valuta['symbol'] == x.wallet_coin:
+                            #print("currency: "+valuta['symbol']+ " cijena: " + valuta['price_usd'])
+                            worth_per_coin = (float(valuta['price_usd']) * float(x.wallet_amount))
+                            wallet_sum += worth_per_coin
+                            wallet_sum_per_coin.update({x.wallet_coin:worth_per_coin})
+
+                            #print("Imas: "+str([x])+ " i to je: " + str(vrijednost))
+                        #    print(worth_per_coin)
+                wallet_sum = round(wallet_sum,2)
 
             queryWallet = CoinWallet.objects.filter(wallet_user=request.user)
             return render(request, 'coinwallet/coinwallet_dashboard.html', {
             'queryWallet': queryWallet,
+            'wallet_sum':wallet_sum,
+            'wallet_sum_per_coin':wallet_sum_per_coin,
             })
     else:
-        return render(
-        request,
-        'coinwallet/coinwallet_dashboard.html'
-    )
+        return render(request,'perasis/not_authenticaded.html')
+
+#def CoinWalletDashboardView(request):
+#    if request.user.is_authenticated():
+#        if request.method == 'POST':
+#            pass
+
+
+
+#        else:
+#            queryWallet = CoinWallet.objects.filter(wallet_user=request.user)
+#            return render(request, 'coinwallet/coinwallet_dashboard.html', {
+#            'queryWallet': queryWallet,
+#            })
+#    else:
+#        return render(request,'perasis/not_authenticaded.html')
+
+
+
+
 
 
 def CoinWalletCreateView(request):
@@ -73,39 +117,3 @@ def CoinWalletDeleteView(request,id):
             return render(request,'perasis/not_owner.html')
     else:
         return render(request,'perasis/not_authenticaded.html')
-
-
-import os
-import json
-import time
-coin_wallet={'XLM':41.958,'MIOTA':26.973,'COSS':49,'APPC':11.98}
-def read_prices_for_my_wallet(self):
-    #code here
-
-    os.system("curl -X GET https://api.coinmarketcap.com/v1/ticker/?limit=0 > prices.txt")
-
-    wallet_sum=0
-    with open('prices.txt', 'r') as content_file:
-        content = content_file.read()
-        sve = json.loads(content)
-        #print(sve)
-        print(coin_wallet)
-        for x in coin_wallet:
-        #    print([x])
-        #    print(coin_wallet[x])
-            for valuta in sve:
-                if valuta['symbol'] == x:
-                    print("currency: "+valuta['symbol']+ " cijena: " + valuta['price_usd'])
-                    #vrijednost = ((valuta['price_usd']) )
-                #    print( (coin_wallet[x]))
-                    #print(vrijednost)
-                    vrijednost = (float(valuta['price_usd']) * float(coin_wallet[x]))
-                    wallet_sum += vrijednost
-                    #print("Imas: "+str([x])+ " i to je: " + str(vrijednost))
-                    print(vrijednost)
-        print("sve skupa: "+ "%.2f" % round(wallet_sum,2) )
-
-    time.sleep(60)
-
-#while True:
-#    read_prices_for_my_wallet(coin_wallet)
