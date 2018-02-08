7d03cc46e1a67a27a63185adefd36d85f060e19e
Goran <goranaviani@gmail.com>
2018-02-08 17:31:34 +0100

    8 februari 2018

--- coinwallet/views.py
+++ coinwallet/views.py
@@ -9,56 +9,74 @@ import time
 from coinwallet.forms import (
 CoinWalletCreateForm,
 )
-
-
 # Create your views here.
+class CryptoProtfolioValue():
+    def __init__(self, prices_json,wallet_sum,wallet_sum_per_coin):
+        self.prices_json=prices_json
+        self.wallet_sum=wallet_sum
+        self.wallet_sum_per_coin=wallet_sum_per_coin
+    def get_prices(self,):
+        prices = os.popen("curl -X GET https://api.coinmarketcap.com/v1/ticker/?limit=0").read()
+        self.prices_json = json.loads(prices)
+        #time.sleep(5)
+    def print_my_portfolio_value(self,):
+        wallet_sum_per_coin={}
+        #queryCoinWallet_model=CoinWallet.objects.filter(wallet_user=request.user)
+        queryCoinWallet_model = CoinWallet.objects.all() # ovde dohvaca sve , treba dohvacat samo od usera
+        for x in queryCoinWallet_model:
+            for valuta in self.prices_json:
+                if valuta['symbol'] == x.wallet_coin:
+                    worth_per_coin = (float(valuta['price_usd']) * float(x.wallet_amount))
+                    self.wallet_sum += worth_per_coin
+                    self.wallet_sum_per_coin.update({x.wallet_coin:worth_per_coin})
+        self.wallet_sum = round(self.wallet_sum,2)
+
 def CoinWalletDashboardView(request):
     if request.user.is_authenticated():
-            os.system("curl -X GET https://api.coinmarketcap.com/v1/ticker/?limit=0 > prices.txt")
-            wallet_sum=0
-            wallet_sum_per_coin={}
-            with open('prices.txt', 'r') as content_file:
-                content = content_file.read()
-
-                sve = json.loads(content)
-                queryCoinWallet_model = CoinWallet.objects.all()
-                for x in queryCoinWallet_model:
-                #    print(x)
-                    for valuta in sve:
-                        #print(valuta['symbol'])
-                        if valuta['symbol'] == x.wallet_coin:
-                            #print("currency: "+valuta['symbol']+ " cijena: " + valuta['price_usd'])
-                            worth_per_coin = (float(valuta['price_usd']) * float(x.wallet_amount))
-                            wallet_sum += worth_per_coin
-                            wallet_sum_per_coin.update({x.wallet_coin:worth_per_coin})
-
-                            #print("Imas: "+str([x])+ " i to je: " + str(vrijednost))
-                        #    print(worth_per_coin)
-                wallet_sum = round(wallet_sum,2)
-
-            queryWallet = CoinWallet.objects.filter(wallet_user=request.user)
-            return render(request, 'coinwallet/coinwallet_dashboard.html', {
-            'queryWallet': queryWallet,
-            'wallet_sum':wallet_sum,
-            'wallet_sum_per_coin':wallet_sum_per_coin,
-            })
+        Cryptotask = CryptoProtfolioValue("",0,{})
+        #while True:
+        Cryptotask.get_prices()
+        Cryptotask.print_my_portfolio_value()
+        queryWallet = CoinWallet.objects.filter(wallet_user=request.user)
+        return render(request, 'coinwallet/coinwallet_dashboard.html', {
+        'queryWallet': queryWallet,
+        'wallet_sum':Cryptotask.wallet_sum,
+        'wallet_sum_per_coin':Cryptotask.wallet_sum_per_coin,
+        })
     else:
         return render(request,'perasis/not_authenticaded.html')
 
+
+#old way:
 #def CoinWalletDashboardView(request):
 #    if request.user.is_authenticated():
-#        if request.method == 'POST':
-#            pass
+#            prices = os.popen("curl -X GET https://api.coinmarketcap.com/v1/ticker/?limit=0").read()
+#            wallet_sum=0
+#            wallet_sum_per_coin={}
+#            sve = json.loads(prices)
+#            queryCoinWallet_model = CoinWallet.objects.all()
+#            for x in queryCoinWallet_model:
+#                for valuta in sve:
+#                    if valuta['symbol'] == x.wallet_coin:
+#                        #print("currency: "+valuta['symbol']+ " cijena: " + valuta['price_usd'])
+#                        worth_per_coin = (float(valuta['price_usd']) * float(x.wallet_amount))
+#                        wallet_sum += worth_per_coin
+#                        wallet_sum_per_coin.update({x.wallet_coin:worth_per_coin})
 
+                        #print("Imas: "+str([x])+ " i to je: " + str(vrijednost))
+                    #    print(worth_per_coin)
+#            wallet_sum = round(wallet_sum,2)
 
-
-#        else:
 #            queryWallet = CoinWallet.objects.filter(wallet_user=request.user)
 #            return render(request, 'coinwallet/coinwallet_dashboard.html', {
 #            'queryWallet': queryWallet,
+#            'wallet_sum':wallet_sum,
+#            'wallet_sum_per_coin':wallet_sum_per_coin,
 #            })
 #    else:
 #        return render(request,'perasis/not_authenticaded.html')
+
+
 
 
 
