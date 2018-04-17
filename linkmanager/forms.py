af51833f11b621d8c3308f4625826c9d5aef9a81
Goran <goranaviani@gmail.com>
2018-04-17 19:17:54 +0200

    fixed register ui

--- linkmanager/forms.py
+++ linkmanager/forms.py
@@ -62,20 +62,22 @@ class NoteDeleteForm(forms.ModelForm):
             )
 
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(help_text = "Requireddd. 150 charachets of fever. Letters, digits and @/./+/-/_ only.")
+    username = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}))
     #first_name = forms.CharField(help_text='Required')
     #last_name = forms.CharField(help_text='Required')
-    email = forms.EmailField(max_length=254, help_text='Required. Type a valid email address.')
-    password1 = forms.CharField(max_length=32, widget=forms.PasswordInput,help_text='Your password cant be too similar to your other personal information,must contain at least 8 characters.')
-    model = User
-    fields = (
-        'username',
-        #'first_name',
-        #'last_name',
-        'email',
-        'password1',
-        'password2',
-        )
+    email = forms.EmailField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}))
+    password1 = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}))
+    password2 = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'note-title-input'}))
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            #'first_name',
+            #'last_name',
+            'email',
+            'password1',
+            'password2',
+            )
 
 class UserInfo(forms.ModelForm):
     class Meta:
