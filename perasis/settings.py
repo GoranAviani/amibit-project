aea8cc3ef1e8a42a2503fe027b8827c4eacd5e01
goranaviani <goranaviani@gmail.com>
2018-11-04 21:13:23 +0100

    test settings

--- perasis/settings.py
+++ perasis/settings.py
@@ -23,8 +23,8 @@ DATABASES = {
         'NAME': 'amibitdb',
         'USER': 'amibitadmin',
         'PASSWORD': '224466integer',
-        #'HOST':'localhost',
-        'HOST': '127.0.0.1',
+        'HOST':'localhost',
+        #'HOST': '127.0.0.1',
         'PORT': '',
     }
  } 
